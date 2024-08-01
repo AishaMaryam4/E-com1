4c219993003a62dc0c9746341df59310c56e3ca7
AishaMaryam <142927390+AishaMaryam4@users.noreply.github.com>
2024-08-01 20:44:05 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import 'font-awesome/css/font-awesome.min.css';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Dashboard from './pages/dashboard/dashboard.component';
+
+function App() {
+  return (
+    <Dashboard />
+  );
+}
+
+export default App;
